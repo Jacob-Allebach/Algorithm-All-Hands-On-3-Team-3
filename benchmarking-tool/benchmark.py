af9606badeb3d5d06d22e7fd1b10f57b9672a65c
Jacob-Allebach <allebach01@allegheny.edu>
2024-04-02 23:14:25 -0400

    Add basic bubble sort and execute function methods

---
+++ benchmarking-tool/benchmark.py
@@ -0,0 +1,7 @@
+def execute_function(file, function_name, list):
+    with open(file) as f:
+        command = f"{f.read()}\n{function_name}({str(list)})"
+        return exec(command)
+
+new_list = [24,6,547,2,5,83,3546,7,35,26,2626,215]
+execute_function("../sorting-algorithms/bubble-sort.py","bubblesort",new_list)
