c7753f7c2e7271754b75149009569c62bdd10f88
AlishChhetri <chhetri01@allegheny.edu>
2024-04-10 09:18:56 -0400

    feat: added calls for file and function

--- allhands/allhands/main.py
+++ allhands/allhands/main.py
@@ -10,10 +10,18 @@ cli = typer.Typer()
 console = Console()
 
 
+def execute_function(file, function_name, arg):
+    with open(file) as f:
+        exec(f.read(), globals())
+        return globals().get(function_name, lambda _: None)(arg)
+
+
 @cli.command()
 def allhands(
     starting_size: int = typer.Option(100),
     number_doubles: int = typer.Option(5),
+    file: str = typer.Option("./allhands/sorting.py"),
+    function_name: str = typer.Option("bubble_sort"),
 ) -> None:
     """Conduct a doubling experiment to measure the performance of list sorting for various algorithms."""
     console.print(
@@ -23,27 +31,24 @@ def allhands(
     console.print(f"Number of doubles to execute: {number_doubles}\n")
     console.print("📈 Here are the results from running the experiment!\n")
 
-    approaches = list(approach.AllhandsApproach)
     all_results = []
 
-    for approach_enum in approaches:
-        algorithm = f"{approach_enum.value}_sort"
-        max_times = []
+    for i in range(number_doubles):
+        size = starting_size * (2**i)
+        data_to_sort = generate.generate_random_container(size)
+        performance_data = benchmark.run_sorting_algorithm(
+            function_name, data_to_sort
+        )
+        max_time = max(performance_data)
+        max_times = [float(max_time)]
 
-        for i in range(number_doubles):
-            size = starting_size * (2**i)
-            performance_data = benchmark.run_sorting_algorithm(
-                algorithm, generate.generate_random_container(size)
-            )
-            max_time = max(performance_data)
-            max_times.append(float(max_time))
+        execute_function(file, function_name, data_to_sort)
 
         all_results.append(max_times)
 
-    header = ["Input Size"] + [approach.value for approach in approaches]
+    header = ["Input Size", function_name]
     data = [
-        [starting_size * (2**i)] + [results[i] for results in all_results]
-        for i in range(number_doubles)
+        [starting_size * (2**i), results[0]] for i, results in enumerate(all_results)
     ]
 
     table = tabulate(
@@ -54,17 +59,16 @@ def allhands(
     # plot
     fig = make_subplots(rows=1, cols=1)
 
-    for i, approach_enum in enumerate(approaches):
-        trace = go.Scatter(
-            x=[starting_size * (2**i) for i in range(number_doubles)],
-            y=all_results[i],
-            mode="lines+markers",
-            name=approach_enum.value,
-        )
-        fig.add_trace(trace)
+    trace = go.Scatter(
+        x=[starting_size * (2**i) for i in range(number_doubles)],
+        y=[results[0] for results in all_results],
+        mode="lines+markers",
+        name=function_name,
+    )
+    fig.add_trace(trace)
 
     fig.update_layout(
-        title="Evaluating the Performance of Sorting Algorithms",
+        title=f"Evaluating the Performance of {function_name}",
         xaxis_title="Input Size",
         yaxis_title="Execution Time (s)",
         showlegend=True,
